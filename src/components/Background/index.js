7899cb9680b7717867ca0d823b5b2abc8f11faf3
Mateus Giroletti <mateusgiroletti97@gmail.com>
2022-02-28 18:06:31 -0300

    Adding all files

---
+++ src/components/Background/index.js
@@ -0,0 +1,8 @@
+import LinearGradient from 'react-native-linear-gradient';
+import styled from 'styled-components/native';
+
+export default styled(LinearGradient).attrs({
+    colors: ['#4b7bec', '#3867d6'],
+})`
+    flex: 1;
+`;
